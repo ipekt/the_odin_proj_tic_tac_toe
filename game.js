5adddc1c25c6a00b57c199ea1358b19bbb21a984
Ipek Turgul <ipek.turgul@gmail.com>
2015-09-21 23:37:43 +1000

    create board with riot

--- game.js
+++ game.js
@@ -1,26 +1,70 @@
+riot.tag('game', '<h1>{ opts.title }</h1> <h2>This game is for 2 player. Please select first player:</h2> <div class="sides"> <div class="nought" onclick="{ setUser }">{ sides.noughts }</div> <div class="cross" onclick="{ setUser }">{ sides.crosses }</div> <button onclick="{ reset }">Reset</button> </div> <ul class="board" id="tst"> <li each="{ key, value in opts.board }" data="{ key }" onclick="{ parent.play }" >{ value }</li> </ul>', function(opts) {
+    this.sides = opts.sides;
+    var count = 1,
+    self = this,
+    userSet = false,
+    user = [];
 
-riot.tag('game', '<h1>{ opts.title }</h1> <h2>hi</h2> <div class="nought">{ opts.sides.noughts }</div> <div class="cross">{ opts.sides.crosses }</div> <div class="board">{ why }</div>', function(opts) {
+    this.setUser = function(e) {
+      var firstUser;
 
-});
+      e.target.classList.add('firstPlayer');
+      if (!userSet) {
+        firstUser = e.target.innerHTML;
+        user.push(firstUser);
 
-riot.tag('script', '', function(opts) {
-  this.sides = opts.sides
-  this.render = function() {
-    var column = ''
-    var row = ''
-    for (var i = 1; i < 3; i++) {
-      column += 'what'
+        if (firstUser == 'X') {
+          user.push('O');
+        } else {
+          user.push('X');
+        }
+      }
+      
+      userSet = true; 
+      console.log(user);
+    }.bind(this);
+
+    var playTurn = function (user, e) {
+
+      e.item.value = user;
+      
+      var board = document.getElementsByClassName('board');
+      e.target.className = 'addcolor';
+      count += 1;
     }
 
-    for (var j = 1; j < 3; j++) {
-      row += 'why' + column + 'whyyy'
-    }
-    return row;
-  }.bind(this);
 
-  this.why = function() {
-    return 'why'
-  }.bind(this);
+    this.play = function(e) {
+      if (count >= 9) {
+        console.log('game over');
+      }
+      if (user.length == 2) {
+        if (count % 2 == 0) {
+          playTurn(user[1], e);
+          return;
+        }
+        playTurn(user[0], e);
+      }
+      this.on('update', function(){
+        var tst = document.getElementById('tst');
 
+      });
+    }.bind(this);
 
-});
+    this.reset = function(e) {
+      user = [];
+      count = 1;
+      userSet = false;
+      var firstPlayer = document.getElementsByClassName('firstPlayer');
+      firstPlayer[0].classList.remove('firstPlayer');
+
+      var boardSections = document.querySelectorAll('.board .addcolor');
+      for (var i = 0; i < boardSections.length; i++) {
+        boardSections[i].classList.remove('addcolor');
+        boardSections[i].innerHTML = '.';
+
+      }
+    }.bind(this);
+
+  
+});  
--- game.tag
+++ game.tag
@@ -1,30 +1,85 @@
-
 <game>
- <h1>{ opts.title }</h1>
-  <h2>hi</h2>
-  <div class="nought">{ opts.sides.noughts }</div>
-  <div class="cross">{ opts.sides.crosses }</div>
+  <h1>{ opts.title }</h1>
+  <h2>This game is for 2 player. Please select first player:</h2>
+  <div class="sides">
+    <div class="nought" onclick={ setUser }>{ sides.noughts }</div>
+    <div class="cross" onclick={ setUser }>{ sides.crosses }</div>
+    <button onclick={ reset }>Reset</button>
 
-  <div class="board">{ why }</div>
-</game>
+  </div>
+  <ul class="board" id="tst">
+    
+    <li each="{ key, value in opts.board }" data="{ key }" onclick={ parent.play } >{ value }</li>
+  </ul>
 
-<script>
-  this.sides = opts.sides
-  render() {
-    var column = ''
-    var row = ''
-    for (var i = 1; i < 3; i++) {
-      column += 'what'
+  <script>
+    this.sides = opts.sides;
+    var count = 1,
+    self = this,
+    userSet = false,
+    user = [];
+
+    setUser(e) {
+      var firstUser;
+
+      e.target.classList.add('firstPlayer');
+      if (!userSet) {
+        firstUser = e.target.innerHTML;
+        user.push(firstUser);
+
+        if (firstUser == 'X') {
+          user.push('O');
+        } else {
+          user.push('X');
+        }
+      }
+      
+      userSet = true; 
+      console.log(user);
     }
 
-    for (var j = 1; j < 3; j++) {
-      row += 'why' + column + 'whyyy'
+    var playTurn = function (user, e) {
+      //e.target.innerHTML = user;
+      e.item.value = user;
+      
+      var board = document.getElementsByClassName('board');
+      e.target.className = 'addcolor';
+      count += 1;
     }
-    return row;
-  }
 
-  why() {
-    return 'why'
-  }
 
-</script>
+    play(e) {
+      if (count >= 9) {
+        console.log('game over');
+      }
+      if (user.length == 2) {
+        if (count % 2 == 0) {
+          playTurn(user[1], e);
+          return;
+        }
+        playTurn(user[0], e);
+      }
+      this.on('update', function(){
+        var tst = document.getElementById('tst');
+        //tst.update();
+      });
+    }
+
+    reset(e) {
+      user = [];
+      count = 1;
+      userSet = false;
+      var firstPlayer = document.getElementsByClassName('firstPlayer');
+      firstPlayer[0].classList.remove('firstPlayer');
+
+      var boardSections = document.querySelectorAll('.board .addcolor');
+      for (var i = 0; i < boardSections.length; i++) {
+        boardSections[i].classList.remove('addcolor');
+        boardSections[i].innerHTML = '.';
+
+      }
+    }
+
+  </script>
+
+</game>  
